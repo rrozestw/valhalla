a73190bd6525e0c3b744497ea6f645e2b5701c7b
Maksim Skurydzin <maksim.skurydzin@mapbox.com>
2020-01-06 14:35:19 +0300

    sequence

--- test/sequence.cc
+++ test/sequence.cc
@@ -1,8 +1,12 @@
 #include "midgard/sequence.h"
-#include "test.h"
 #include <cstdint>
 
+#include <gtest/gtest.h>
+
+
 using namespace valhalla::midgard;
+
+namespace {
 
 struct osm_node {
   uint64_t id;
@@ -19,20 +23,19 @@ std::string write_nodes(const uint64_t count) {
   return file_name;
 }
 
-void sort_nodes(const std::string& file_name) {
+void sort_nodes(const std::string &file_name) {
   sequence<osm_node> sequence(file_name, false, 512);
-  sequence.sort([](const osm_node& a, const osm_node& b) { return a.id < b.id; });
+  sequence.sort([](const osm_node &a, const osm_node &b) { return a.id < b.id; });
 }
 
-void read_nodes(const std::string& file_name, const uint64_t count) {
+void read_nodes(const std::string &file_name, const uint64_t count) {
   sequence<osm_node> sequence(file_name, false, 512);
-  auto less_than = [](const osm_node& a, const osm_node& b) { return a.id < b.id; };
+  auto less_than = [](const osm_node &a, const osm_node &b) { return a.id < b.id; };
   for (uint64_t i = 0; i < count; ++i) {
     // grab an element
     auto element = sequence[i];
     osm_node node = *element;
-    if (node.id != i)
-      throw std::runtime_error("Found wrong node at: " + std::to_string(i));
+    ASSERT_EQ(node.id , i) << "Found wrong node at: " + std::to_string(i);
     // change it
     node.lat = 7;
     node.lng = 7;
@@ -40,44 +43,38 @@ void read_nodes(const std::string& file_name, const uint64_t count) {
     element = node;
     // find the same element with binary search (far slower)
     osm_node target{i};
-    if (sequence.find(target, less_than) == sequence.end())
-      throw std::runtime_error("Didn't find node " + std::to_string(i));
+    ASSERT_NE(sequence.find(target, less_than), sequence.end())
+      << "Didn't find node " + std::to_string(i);
   }
 }
 
-void test_read_write() {
+TEST(Sequence, ReadWrite) {
   size_t count = 1024;
   auto file_name = write_nodes(count);
   sort_nodes(file_name);
   read_nodes(file_name, count);
 }
 
-void test_iterator() {
+TEST(Sequence, Iterator) {
   sequence<osm_node> sequence("nodes.nd", false, 512);
   auto i = sequence.begin();
   auto j = i + 1;
-  if (j.position() != 1)
-    throw std::runtime_error("Plus operator wasn't right");
+  EXPECT_EQ(j.position() , 1) << "Plus operator wasn't right";
 
   ++i;
-  if (i.position() != 1)
-    throw std::runtime_error("Pre-increment operator wasn't right");
+  EXPECT_EQ(i.position() , 1) << "Pre-increment operator wasn't right";
 
   auto k = i - 1;
-  if (k.position() != 0)
-    throw std::runtime_error("Minus operator wasn't right");
+  EXPECT_EQ(k.position() , 0) << "Minus operator wasn't right";
 
   --i;
-  if (i.position() != 0)
-    throw std::runtime_error("Pre-decrement operator wasn't right");
+  EXPECT_EQ(i.position() , 0) << "Pre-decrement operator wasn't right";
 }
 
-int main() {
-  test::suite suite("sequence");
+} // namespace
 
-  suite.test(TEST_CASE(test_read_write));
 
-  suite.test(TEST_CASE(test_iterator));
-
-  return suite.tear_down();
+int main(int argc, char* argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
